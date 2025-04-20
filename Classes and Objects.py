af0c8e02cde4829edf5730a33c35bd2bf1383cf1
Aryan160512 <157271425+Aryan160512@users.noreply.github.com>
2025-04-20 11:00:25 +0100

    Add files via upload

--- Classes and Objects.py
+++ Classes and Objects.py
@@ -6,15 +6,46 @@ class Student():
         self.age = age
         self.height = height
         self.year = year
-        self.marks = []
+        self._marks = []
 
     def displayDetails(self):
         print(self.name)
         print(self.age)
         print(self.height)
         print(self.year)
-        print(self.marks)
+        print(self._marks)
+    
+    def updateMarks(self):
+        noOfSubjects = int(input('How many subjects does student take?'))
+        for i in range(noOfSubjects):
+
+            markForSubject = int(input(f'Enter your mark for Subject {i + 1}: '))
+            self._marks.append(markForSubject)
+
+    def markCalculations(self):
+        sum = 0
+        for eachMark in self._marks:
+            sum = sum + eachMark 
+        average = sum / len(self._marks)
+        print(average)
+        print(sum)
+
+    def displayMarks(self):
+        for i in self._marks:
+            print(i)
 
 #Creating Objects
 studentDetail = Student('Aryan', 12, '160 cm', 8)
 studentDetail.displayDetails()
+studentDetail.updateMarks()
+studentDetail.markCalculations()
+studentDetail.displayMarks()
+
+#Accessing or Changing the Object
+studentDetail.age = 13
+print(studentDetail.age)
+
+#studentDetail.marks = ['10', 15, 'happy']
+#studentDetail.markCalculations()
+
+#
